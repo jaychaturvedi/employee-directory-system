98240c9b3545ae72b03f9abd071074477349227e
Jay Chaturvedi <jaychaturvedi18@gmail.com>
2023-07-11 23:27:34 +0530

    added catch async wrapper

---
+++ backend/src/utils/catchAsync.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function catchAsync(fn: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
